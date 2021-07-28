a791fab70e19128a3d9b83e35eb6ddb25fd84276
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2021-07-28 22:31:35 +0900

    Apply ArticulationBody

--- Backhoe/Backhoe.cs
+++ Backhoe/Backhoe.cs
@@ -6,35 +6,41 @@ namespace Ocs.Vehicles
 {
     public class Backhoe : MonoBehaviour
     {
-        [SerializeField] private JointInfo _base;
-        [SerializeField] private JointInfo _boom;
-        [SerializeField] private JointInfo _arm;
-        [SerializeField] private JointInfo _end;
+        [SerializeField] private ArticulationInfo _base;
+        [SerializeField] private ArticulationInfo _boom;
+        [SerializeField] private ArticulationInfo _arm;
+        [SerializeField] private ArticulationInfo _end;
 
         private float _baseJointInput;
         private float _boomJointInput;
         private float _armJointInput;
         private float _endJointInput;
 
-        public float BaseJointInput { get => _baseJointInput; set => _baseJointInput = value; }
-        public float BoomJointInput { get => _boomJointInput; set => _boomJointInput = value; }
-        public float ArmJointInput { get => _armJointInput; set => _armJointInput = value; }
-        public float EndJointInput { get => _endJointInput; set => _endJointInput = value; }
+        public ArticulationInfo Base { get => _base; }
+        public ArticulationInfo Boom { get => _boom; }
+        public ArticulationInfo Arm { get => _arm; }
+        public ArticulationInfo End { get => _end; }
 
-        private void Awake()
+        public float BaseJointInput { set => _baseJointInput = value; }
+        public float BoomJointInput { set => _boomJointInput = value; }
+        public float ArmJointInput { set => _armJointInput = value; }
+        public float EndJointInput { set => _endJointInput = value; }
+
+        private void Start()
         {
-            this._boom.joint.parent = this._base.joint;
-            this._arm.joint.parent = this._boom.joint;
-            this._end.joint.parent = this._arm.joint;
+            this._base.Initialize();
+            this._boom.Initialize();
+            this._arm.Initialize();
+            this._end.Initialize();
         }
 
         // Update is called once per frame
         void Update()
         {
-            this._base.Rotate(this._baseJointInput / 10);
-            this._boom.Rotate(this._boomJointInput / 10);
-            this._arm.Rotate(this._armJointInput / 10);
-            this._end.Rotate(this._endJointInput / 10);
+            this._base.Rotate(this._baseJointInput);
+            this._boom.Rotate(this._boomJointInput);
+            this._arm.Rotate(this._armJointInput);
+            this._end.Rotate(this._endJointInput);
         }
     }
 }
